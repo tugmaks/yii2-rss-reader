d055d0b1cafdf4d04c69e65dcaa4344cd6153262
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-08 13:08:18 +0400

    devstart

--- README.md
+++ README.md
@@ -28,4 +28,6 @@ Usage
 Once the extension is installed, simply use it in your code by  :
 
 ```php
-<?= \tugmaks\RssReader\AutoloadExample::widget(); ?>```
+<?= \tugmaks\RssFeed\RssReader::widget([
+'channel'=>'http://example.com/feed.xml'
+]); ?>```
--- RssReader.php
+++ RssReader.php
@@ -6,10 +6,15 @@ namespace tugmaks\RssFeed;
  * This is just an example.
  */
 class RssReader extends \yii\base\Widget {
+
     public $channel;
 
     public function run() {
-        return "Hello!";
+        $xml = @simplexml_load_file($this->channel);
+        if ($xml === false) {
+            die('Error parse RSS: ' . $rss);
+        }
+        var_dump($xml);
     }
 
 }
