e55bb3fbdc7ffa34bf1b20887e7bef1d295838e0
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-08 14:46:16 +0400

    done&

--- views/wrap.php
+++ views/wrap.php
@@ -3,5 +3,5 @@
 use yii\helpers\Html;
 use yii\widgets\ListView;
 
-$content = ListView::widget(['dataProvider' => $provider, 'itemView' => $this->context->itemView, 'layout' => '{items}\n{pager}']);
+$content = ListView::widget(['dataProvider' => $provider, 'itemView' => $this->context->itemView, 'layout' => "{items} \n {pager}"]);
 echo Html::tag($this->context->wrapTag, $content, ['class' => $this->context->wrapClass]);
