7befb5eeaa0dd5716339f6442138fb4cde5fde13
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-08 12:53:47 +0400

    devstart

---
+++ RssReader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tugmaks\RssFeed;
+
+/**
+ * This is just an example.
+ */
+class RssReader extends \yii\base\Widget {
+    public $channel;
+
+    public function run() {
+        return "Hello!";
+    }
+
+}
