d99ab65eaae173cf87d0aa303d5f837fce17cc42
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-08 13:37:00 +0400

    devstart

--- views/wrap.php
+++ views/wrap.php
@@ -3,5 +3,6 @@
 use yii\helpers\Html;
 use yii\widgets\ListView;
 
+
 $content = ListView::widget(['dataProvider' => $provider, 'itemView' => $this->context->itemView]);
 echo Html::tag($this->context->wrapTag, $content, ['class' => $this->wrapClass]);
