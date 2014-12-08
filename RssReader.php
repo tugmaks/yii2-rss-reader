0cf92fc0e345a568d7d8743e93ac84e9633757a5
Tugaev Maxim <tugmaks@yandex.ru>
2014-12-08 13:34:43 +0400

    devstart

--- RssReader.php
+++ RssReader.php
@@ -8,13 +8,31 @@ namespace tugmaks\RssFeed;
 class RssReader extends \yii\base\Widget {
 
     public $channel;
+    public $pageSize = 10;
+    public $itemView = 'item';
+    public $wrapTag = 'ul';
+    public $wrapClass = 'rss-wrap';
 
     public function run() {
         $xml = @simplexml_load_file($this->channel);
         if ($xml === false) {
             die('Error parse RSS: ' . $rss);
         }
-        var_dump($xml);
+
+        $provider = new ArrayDataProvider([
+            'allModels' => $xml->channel->item,
+//            'sort' => [
+//                'attributes' => ['id', 'username', 'email'],
+//            ],
+            'pagination' => [
+                'pageSize' => $this->pageSize,
+            ],
+        ]);
+
+
+        return $this->render('wrap', [
+                    'provider' => $provider,
+        ]);
     }
 
 }
---
+++ views/wrap.php
@@ -0,0 +1,7 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+
+$content = ListView::widget(['dataProvider' => $provider, 'itemView' => $this->context->itemView]);
+echo Html::tag($this->context->wrapTag, $content, ['class' => $this->wrapClass]);
